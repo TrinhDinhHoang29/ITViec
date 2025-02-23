f2f202686b70ca331ef1e16b8844f2657c905720
TrinhDinhHoang29 <162282399+TrinhDinhHoang29@users.noreply.github.com>
2025-02-23 21:34:45 +0700

    Update title

--- src/components/InfoCompanySpotLight/index.jsx
+++ src/components/InfoCompanySpotLight/index.jsx
@@ -11,7 +11,7 @@ const InfoCompanySpotlight = () => {
           style={{ width: "150px", height: "150px", borderRadius: "5px" }}
           className="d-flex align-items-center bg-white border"
         >
-          <img src="/images/GrabLogo.webp" alt="" width={"100%"} />
+          <img src="./images/GrabLogo.webp" alt="" width={"100%"} />
         </div>
       </div>
     </>
